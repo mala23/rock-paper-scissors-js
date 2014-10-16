411f66fbcfbe594e3a04d68b6a343d652b9dea91
mala23 <mail@mala23.me>
2014-10-16 17:45:03 +0100

    Add Rules, not finished yet

--- spec/RockPaperScissorsSpec.js
+++ spec/RockPaperScissorsSpec.js
@@ -1,10 +1,20 @@
 describe("Player", function() {
 
-	var player;
-
-	it("should have a gesture", function() {
+	beforeEach(function(){
 		player = new Player();
-		expect(player.gesture()).toBe(true);
+		rock = new Rock();
+		scissors = new Scissors();
 	});
 
+	it("should have a gesture", function() {
+		expect(player.gesture(rock)).toBe(rock);
+	});
+
+});
+
+describe("rules", function() {
+
+	it("a rock should beat scissors", function(){
+		expect(player.gesture(rock)).toBeat(scissors);
+	});
 });
--- src/RockPaperScissors.js
+++ src/RockPaperScissors.js
@@ -1,5 +1,10 @@
 function Player() {};
 
-	Player.prototype.gesture = function() {
-		return true;
+	Player.prototype.gesture = function(choice) {
+		return choice;
 	};
+
+function Rock() {};
+
+function Scissors() {};
+
